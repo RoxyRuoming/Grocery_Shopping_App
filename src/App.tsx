ab9e7e02dc2cad3355e0f92e82c8b89366a0e8c2
RoxyRuoming <zhang.ruom@northeastern.edu>
2023-10-19 18:15:59 -0700

    Ref + CSS: make a welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useRef } from 'react';
+import './styles/app.css';
+
 const App = () => {
-  
-  return(
-    <>
-    <div className="border-bottom">shop for fun</div>
-    </>
-  ) 
+  const ref = useRef<HTMLDivElement>(null!);
+
+  useEffect(() => {
+    ref.current.style.opacity = '1';
+  }, [])
+
+  return (
+    <div ref={ref} className="page guide-page">
+      <img
+        alt="欢乐购"
+        className="main-pic"
+        src={require('./images/halg_logo_icon_@2x.png')}
+      />
+      <p className='title'>欢乐购</p>
+      <img
+        alt="欢乐购"
+        className="sub-pic"
+        src={require('./images/slogn_word_icon_@2x.png')}
+      />
+      <div className="iconfont arrow-icon">&#xe60c;</div>
+    </div>
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import './styles/border.css';
+import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// automatically adjust by the size of the screen / base-screen is IPHONE SE
+document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px';
+window.addEventListener('resize', () => {
+  document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px';
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
